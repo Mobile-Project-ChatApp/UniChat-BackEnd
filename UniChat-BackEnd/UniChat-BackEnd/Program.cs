dd203858b4cd8c50f50435232bff1c33d5f28d5b
Congying Zhao <t3cozh00@students.oamk.fi>
2025-03-17 15:02:23 +0200

    allows external access to the API.

--- UniChat-BackEnd/UniChat-BackEnd/Program.cs
+++ UniChat-BackEnd/UniChat-BackEnd/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.Urls.Add("http://0.0.0.0:5222");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
