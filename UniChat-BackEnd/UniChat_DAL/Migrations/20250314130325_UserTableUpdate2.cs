33c264174f35dae69da797d918d88f0573ee0c6a
Matta5 <mvdheiligenberg2006@gmail.com>
2025-03-17 13:39:38 +0100

    updated migrations

---
+++ UniChat-BackEnd/UniChat_DAL/Migrations/20250314130325_UserTableUpdate2.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniChat_DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserTableUpdate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Semester",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Study",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Semester",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Study",
+                table: "Users");
+        }
+    }
+}
