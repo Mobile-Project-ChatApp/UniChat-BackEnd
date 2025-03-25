1164abb64ff9181e0b95c8039cace8616d1d851a
jayathracm <jayathracm@gmail.com>
2025-03-25 14:59:52 +0200

    fix: change Semester column type from string to integer in Users table

---
+++ UniChat-BackEnd/UniChat_DAL/Migrations/20250325124801_FixSemesterColumn.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniChat_DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSemesterColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Remove the automatic column alteration
+            /*
+            migrationBuilder.AlterColumn<int>(
+                name: "Semester",
+                table: "Users",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+            */
+            
+            // Use raw SQL with USING clause for safe conversion
+            migrationBuilder.Sql("ALTER TABLE \"Users\" ALTER COLUMN \"Semester\" TYPE integer USING CASE WHEN \"Semester\" ~ '^[0-9]+$' THEN \"Semester\"::integer ELSE NULL END;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Semester",
+                table: "Users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
