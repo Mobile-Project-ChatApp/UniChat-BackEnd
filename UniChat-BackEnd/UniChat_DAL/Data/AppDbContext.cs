065d8184136c06322f3b29b409505e92ff7ebd6c
jayathracm <jayathracm@gmail.com>
2025-03-27 11:15:11 +0200

    feat: implement AnnouncementEntity and UserAnnouncementInteraction classes with repository methods for announcements management

--- UniChat-BackEnd/UniChat_DAL/Data/AppDbContext.cs
+++ UniChat-BackEnd/UniChat_DAL/Data/AppDbContext.cs
@@ -8,10 +8,33 @@ namespace UniChat_DAL.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<UserEntity> Users { get; set; }
+        public DbSet<AnnouncementEntity> Announcements { get; set; }
+        public DbSet<UserAnnouncementInteraction> UserAnnouncementInteractions { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserAnnouncementInteraction>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<UserAnnouncementInteraction>()
+                .HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<UserAnnouncementInteraction>()
+                .HasOne(x => x.Announcement)
+                .WithMany(a => a.UserInteractions)
+                .HasForeignKey(x => x.AnnouncementId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<AnnouncementEntity>()
+                .HasIndex(x => x.Date);
+
+            modelBuilder.Entity<AnnouncementEntity>()
+                .HasIndex(x => x.Important);
         }
     }
 }
