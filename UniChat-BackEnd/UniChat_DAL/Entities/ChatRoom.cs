2ff48abbdaa133d6d70db48a8d3e405c8104cc0b
Quin Coolen <quin.coolen@gmail.com>
2025-04-14 09:53:21 +0200

    Update entities and add migrations

--- UniChat-BackEnd/UniChat_DAL/Entities/ChatRoom.cs
+++ UniChat-BackEnd/UniChat_DAL/Entities/ChatRoom.cs
@@ -6,6 +6,9 @@ namespace UniChat_DAL.Entities
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        
+        public List<ChatRoomSemester> ChatRoomSemesters { get; set; }
+        public List<ChatRoomStudy> ChatRoomStudies { get; set; }
         public List<Message> Messages { get; set; }
         public List<UserChatroom> UserChatrooms { get; set; }
         public List<AnnouncementEntity> Announcements { get; set; }
